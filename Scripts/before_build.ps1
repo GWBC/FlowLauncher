34d6ee2523a08af7952d7f7641bcd38426cbfbc8
gwbc <504274168@qq.com>
2025-01-13 18:13:09 +0800

    添加修改版本脚本

---
+++ Scripts/before_build.ps1
@@ -0,0 +1,16 @@
+function Replace-Version($path, $version){
+    $filePath = "$path\SolutionAssemblyInfo.cs"
+    $content = Get-Content -Path $filePath
+    $newContent = $content -replace "0.0.0.0", "$version"
+    Set-Content -Path $filePath -Value $newContent
+    $content = Get-Content -Path $filePath
+    Write-Output "#################################"
+    Write-Output $content -ForegroundColor Green
+    Write-Output "#################################"
+}
+
+nuget restore
+
+$p = Build-Path
+$v = Build-Version
+Copy-Resources $p
